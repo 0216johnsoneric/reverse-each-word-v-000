c9425b4cb1a6dca694ebb2762c7e7b8f6e785fd3
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-02 23:05:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,3 @@
 def reverse(string)
-    reverse = ""
-    index = 0
-    while index < string.length
-       reverse = string[index] + reverse
-       index += 1
-    end
-    return reverse
+    string.split.reverse.join(" ")
 end
