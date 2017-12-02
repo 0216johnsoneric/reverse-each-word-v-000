d0ba6408abbf18114cf29322cf402eae57072921
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-02 22:55:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,11 @@
 def reverse_each_word(string)
   return string.reverse
 end
+
+def reverse_each_word(string)
+  newarray = []
+  array.map! do |string|
+    string.reverse
+  end
+array.join(" ")
+end
